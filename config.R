79a61ba922057355076f3f0a3857a442abd17d56
Vincent van Hoef <vincent.vanhoef@kuleuven.vib.be>
2019-12-03 11:33:27 +0100

    Add analysis

---
+++ config.R
@@ -0,0 +1,18 @@
+library("workflowr")
+
+# Replace the example text with your information
+wflow_git_config(user.name = "Vincent van Hoef", user.email = "vincent.vanhoef@kuleuven.vib.be")
+
+
+wflow_start("/Users/u0049379/Desktop/workflowr_test")
+
+wflow_build()
+
+
+wflow_status()
+
+wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd"),
+              "Publish the initial files for myproject")
+wflow_use_github("vincent-van-hoef")
+wflow_git_push()
+wflow_open("analysis/first-analysis.Rmd")
