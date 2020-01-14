05a916e1f44aae550b94b3f1ff3d7161df0aa0cb
Vincent van Hoef <vincent.vanhoef@kuleuven.vib.be>
2020-01-14 17:06:15 +0100

    test for pull

--- config.R
+++ config.R
@@ -3,10 +3,4 @@ library("workflowr")
 # Replace the example text with your information
 wflow_git_config(user.name = "Vincent van Hoef", user.email = "vincent.vanhoef@kuleuven.vib.be")
 
-wflow_start("/Users/u0049379/Desktop/workflowr_test")
 
-wflow_status()
-
-
-
-add this to test pull
