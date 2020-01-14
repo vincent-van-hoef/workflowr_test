b716de9ad20fb2ec23a0fc649a17c35d0346f6cd
Vincent van Hoef <vincent.vanhoef@kuleuven.vib.be>
2020-01-14 16:25:57 +0100

    remove line

--- config.R
+++ config.R
@@ -6,11 +6,6 @@ wflow_git_config(user.name = "Vincent van Hoef", user.email = "vincent.vanhoef@k
 
 wflow_start("/Users/u0049379/Desktop/workflowr_test")
 
-
-
-wflow_build()
-
-
 wflow_status()
 
 wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd"),
