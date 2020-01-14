9bcb7648738bddbd7098483e9f425d04b2bc8fdf
Vincent van Hoef <vincent.vanhoef@kuleuven.vib.be>
2020-01-14 16:59:57 +0100

    delitions

--- config.R
+++ config.R
@@ -3,7 +3,6 @@ library("workflowr")
 # Replace the example text with your information
 wflow_git_config(user.name = "Vincent van Hoef", user.email = "vincent.vanhoef@kuleuven.vib.be")
 
-
 wflow_start("/Users/u0049379/Desktop/workflowr_test")
 
 wflow_status()
@@ -13,15 +12,3 @@ wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rm
 wflow_use_github("vincent-van-hoef")
 wflow_git_push()
 wflow_open("analysis/first-analysis.Rmd")
-
-
-this is other db
-
-
-
-
-
-this is another db
-
-This is a test
-
