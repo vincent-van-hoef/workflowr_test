5f93476088323d8361e24dde47b4b947402dbff9
Vincent van Hoef <vincent.vanhoef@kuleuven.vib.be>
2020-01-14 17:02:25 +0100

    delete test

--- config.R
+++ config.R
@@ -7,11 +7,3 @@ wflow_start("/Users/u0049379/Desktop/workflowr_test")
 
 wflow_status()
 
-wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd"),
-              "Publish the initial files for myproject")
-wflow_use_github("vincent-van-hoef")
-wflow_git_push()
-wflow_open("analysis/first-analysis.Rmd")
-
-
-What is the workflow for this?
